93bf2bf1c75638e48a583213820c8a50b445daa7
Yashika <80509513+b1902094@users.noreply.github.com>
2021-10-15 11:08:32 +0530

    Create Util.java

---
+++ Util.java
@@ -0,0 +1,15 @@
+package com.vicky.jabazon.Util;
+
+public class Util {
+
+    //Database
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "jabazon_db";
+    public static final String USER_TABLE_NAME = "users";
+
+    //User
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+
+    public static User USER;
+}
