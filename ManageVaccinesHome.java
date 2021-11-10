6cbf8221506f368c2a4e48960cc0d9be0ee53a63
Yashika <80509513+b1902094@users.noreply.github.com>
2021-11-10 06:01:29 +0530

    Update ManageVaccinesHome.java

--- ManageVaccinesHome.java
+++ ManageVaccinesHome.java
@@ -1,85 +1,37 @@
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.vicky.jabazon.Util.Util;
-import com.vicky.jabazon.Util.Vaccines;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ManageVaccinesHome extends AppCompatActivity {
-
-    ListView listViewVaccines;
-    List<Vaccines> vaccinesArray;
-    ListAdapter adapter;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ConstraintLayout constraintLayoutVaccines;
-    FloatingActionButton addVaccine;
-    Vaccines vaccines;
+public class ManageVaccines extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_vaccines_home);
+        setContentView(R.layout.activity_manage_vaccines);
 
-        listViewVaccines = findViewById(R.id.list_view_vaccines_list);
-        constraintLayoutVaccines = findViewById(R.id.layout_vaccines);
-        addVaccine = findViewById(R.id.floating_action_btn_add_vaccine);
+        RecyclerView rv = findViewById(R.id.vaccineList_rv);
 
-        getAllVaccines();
+        VaccineItems[] vid = {
+                new VaccineItems("SIN001", "Sinovac", "Beijing Pharma"),
+                new VaccineItems("AZ0001", "AstraZeneca", "SII"),
+                new VaccineItems("PFI001", "Pfizer", "BioNTech"),
+                new VaccineItems("PFI002", "Pfizer", "BioNTech")
+        };
 
-        addVaccine.setOnClickListener(new View.OnClickListener() {
+        VaccineListAdapter adapter = new VaccineListAdapter(vid);
+        rv.setAdapter(adapter);
+
+        rv.addOnItemTouchListener(new RecyclerItemClickListener(rv, new RecyclerItemClickListener.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ManageVaccinesHome.this, RecordVaccine.class));
-
-            }
-        });
-
-        listViewVaccines.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(ManageVaccinesHome.this, RecordVaccine.class);
-                Vaccines vaccines = vaccinesArray.get(i);
-                intent.putExtra("vacccine", vaccines);
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(ManageVaccines.this, SelectedVid.class);
+                intent.putExtra("Vaccines", vid[position]);
                 startActivity(intent);
             }
-        });
-    }
-
-    private void getAllVaccines(){
-        db.collection("Vaccines")
-                .whereEqualTo("vaccineId", Util.VACCINES.getVaccineID())
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
-                            vaccinesArray = new ArrayList<>();
-                            for (QueryDocumentSnapshot document : task.getResult())
-                                vaccines = document.toObject(Vaccines.class);
-                            vaccinesArray.add(vaccines);
-                        }
-                        adapter = new ArrayAdapter<Vaccines>(ManageVaccinesHome.this, android.R.layout.simple_list_item_1, vaccinesArray);
-                        listViewVaccines.setAdapter(adapter);
-
-                    }
-                });
+        }));
     }
 }
