52768fdf451cb6dd8fbd90a7036fa0b59a0e9383
Yashika <80509513+b1902094@users.noreply.github.com>
2021-11-13 20:11:59 +0530

    Update ManageBatchItems.java

--- ManageBatchItems.java
+++ ManageBatchItems.java
@@ -1,8 +1,9 @@
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +11,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -44,6 +48,47 @@ public class ManageBatchItems extends AppCompatActivity {
                 BatchItems batch = batchArray.get(i);
                 intent.putExtra("Batches", batch);
                 startActivity(intent);
+            }
+        });
+
+        listViewBatches.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(ManageBatchItems.this);
+                alert.setTitle("Delete");
+                alert.setMessage("Do you want to delete this vaccine batch?");
+                alert.setCancelable(true);
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        BatchItems batch = batchArray.get(position);
+                        db.collection("Batches").document(batch.getBatchNo())
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        getCurrentBatchList();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(ManageBatchItems.this,
+                                                e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                });
+
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
+                return true;
             }
         });
         getCurrentBatchList();
