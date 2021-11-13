d74753b795ed48d547030a8f6de1751a1b2a5356
Yashika <80509513+b1902094@users.noreply.github.com>
2021-11-13 11:24:55 +0530

    Update SelectedVid.java

--- SelectedVid.java
+++ SelectedVid.java
@@ -1,161 +1,156 @@
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Calendar;
 
 public class SelectedVid extends AppCompatActivity {
 
-    ImageView imgBtnCal;
-    EditText batchNo, expDate, qtyAvail, qtyAdmin ;
-    boolean isBatchNoValid, isDateValid, isQtyAvailVaild, isQtyAdminValid;
+    ImageButton imgBtnCal;
+    EditText newBatchNo, newBatchExpDate, newBatchQtyAvail, newBatchQtyAdmin ;
+    boolean isBatchNumValid, isExpDateValid, isQtyAvailVaild, isQtyAdminValid;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    Vaccines vaccines;
+    BatchItems batch;
+    Button btnSubmit;
+    ConstraintLayout clNewBatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_vid);
 
-        try {
-            TextView tvName = findViewById(R.id.tv_selected_vName);
-            TextView tvManufacturer = findViewById(R.id.tv_selected_manufacturer);
+        clNewBatch = findViewById(R.id.layout_new_batch);
+        newBatchNo = findViewById(R.id.et_new_batchNo);
+        newBatchExpDate = findViewById(R.id.et_new_expDate);
+        newBatchQtyAvail = findViewById(R.id.et_new_qtyAvail);
+        newBatchQtyAdmin = findViewById(R.id.et_new_qtyAdmin);
 
-            VaccineItems selectedVID = (VaccineItems) getIntent().getSerializableExtra("Vaccines");
-            getSupportActionBar().setTitle(selectedVID.vID);
+        btnSubmit = findViewById(R.id.btn_submit_batch);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!newBatchNo.getText().toString().isEmpty()){
+                    if(batch == null){
+                        DocumentReference newBatchRef = db.collection("Batches").document();
+                        batch = new BatchItems();
+                        batch.setBatchNo(newBatchNo.getText().toString());
+                        batch.setExpDate(newBatchExpDate.getText().toString());
+                        batch.setQtyAdmin(newBatchQtyAdmin.getText().toString());
+                        batch.setQtyAvail(newBatchQtyAvail.getText().toString());
 
-            tvName.setText(selectedVID.vName);
-            tvManufacturer.setText(selectedVID.manuFacturer);
-            EditText batchNo = findViewById(R.id.et_new_batchNo);
-            EditText expDate = findViewById(R.id.et_new_expDate);
-            EditText qtyAvail = findViewById(R.id.et_new_qtyAvail);
-            EditText qtyAdmin = findViewById(R.id.et_new_qtyAdmin);
-
-            imgBtnCal = findViewById(R.id.img_btn_calender);
-            imgBtnCal.setOnClickListener(v -> {
-                Calendar calendar = Calendar.getInstance();
-                int todayYear = calendar.get(Calendar.YEAR);
-                int todayMonth = calendar.get(Calendar.MONTH);
-                int todayDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(SelectedVid.this,
-                        (view, year1, month1, dayOfMonth1) -> expDate.setText(dayOfMonth1 + "/" + (month1 + 1) + "/" + year1),
-                        todayYear, todayMonth, todayDayOfMonth);
-                datePickerDialog.getDatePicker();
-                datePickerDialog.show();
-
-            });
-            Button btnSubmit = findViewById(R.id.btn_submit);
-            btnSubmit.setOnClickListener(view -> {
-                dataValidation2();
-            });
-        } catch (Exception exception) {
-            Toast.makeText(SelectedVid.this, exception.getMessage(), Toast.LENGTH_LONG).show();
-
-            Button btnSubmit = findViewById(R.id.btn_submit);
-            btnSubmit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (!batchNo.getText().toString().isEmpty()) {
-                        if (vaccines == null) {
-                            DocumentReference newVacRef = db.collection("New Batch").document();
-                            vaccines = new Vaccines();
-                            vaccines.setVaccineID(newVacRef.getId());
-                            vaccines.setBatchNo(batchNo.getText().toString());
-                            vaccines.setExpiryDate(expDate.getText().toString());
-                            vaccines.setQuantityAvailable(qtyAvail.getText().toString());
-                            vaccines.setQuantityAdministered(qtyAdmin.getText().toString());
-
-                            db.collection("New Batch")
-                                    .document(newVacRef.getId())
-                                    .set(vaccines)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void unused) {
-                                            Toast.makeText(SelectedVid.this, "Successfully added!", Toast.LENGTH_SHORT).show();
-                                            finish();
-                                        }
-                                    })
-                                    .addOnFailureListener((new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(SelectedVid.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                                        }
-                                    }));
-                        } else {
-                            DocumentReference vacRef = db.collection("New Batch")
-                                    .document(vaccines.getVaccineID());
-                            vacRef.update(
-                                    "batchNo", batchNo.getText().toString(),
-                                    "expDate", expDate.getText().toString(),
-                                    "qtyAvail", qtyAvail.getText().toString(),
-                                    "qtyAdmin", qtyAdmin.getText().toString())
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void unused) {
-                                            finish();
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(SelectedVid.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-                        }
+                        db.collection("Batches")
+                                .document(batch.getBatchNo())
+                                .set(batch)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        finish();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(SelectedVid.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        DocumentReference BatchRef = db.collection("Batches")
+                                .document(batch.getBatchNo());
+                        BatchRef.update("batchNo", newBatchNo.getText().toString(),
+                                "expDate", newBatchExpDate.getText().toString(),
+                                "qtyAdmin", newBatchQtyAdmin.getText().toString(),
+                                "qtyAvail", newBatchQtyAvail.getText().toString())
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        finish();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(SelectedVid.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
                     }
+                    dataValidation();
                 }
-            });
-        }
+            }
+        });
+
+        imgBtnCal = findViewById(R.id.img_btn_selectedvid_calender);
+        imgBtnCal.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            int todayYear = calendar.get(Calendar.YEAR);
+            int todayMonth = calendar.get(Calendar.MONTH);
+            int todayDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(SelectedVid.this,
+                    (view, year1, month1, dayOfMonth1) -> newBatchExpDate.setText(dayOfMonth1 + "/" + (month1+1) + "/" + year1),
+                    todayYear, todayMonth, todayDayOfMonth);
+            datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+            datePickerDialog.show();
+
+        });
+
     }
 
-    private void dataValidation2() {
-        if(batchNo.getText() == null){
-            isBatchNoValid = false;
-        } else{
-            isBatchNoValid = true;
+    private void dataValidation() {
+        if(newBatchNo.getText() == null){
+            isBatchNumValid = false;
         }
-        if(expDate.getText() == null){
-            isDateValid = false;
-        } else{
-            isDateValid = true;
+        else{
+            isBatchNumValid = true;
         }
-        if(qtyAvail.getText() == null){
-            isQtyAvailVaild = false;
-        } else{
-            isQtyAvailVaild = true;
+        if(newBatchExpDate.getText().toString().isEmpty()){
+            isExpDateValid = false;
+        }else{
+            isExpDateValid = true;
         }
-        if(qtyAdmin.getText() == null){
+        if(newBatchQtyAdmin.getText() == null){
             isQtyAdminValid = false;
-        } else{
+        }else{
             isQtyAdminValid = true;
         }
+        if(newBatchQtyAvail.getText() == null){
+            isQtyAvailVaild = false;
+        }else{
+            isQtyAvailVaild = true;
+        }
 
-        if( isBatchNoValid && isDateValid && isQtyAvailVaild && isQtyAdminValid) {
-            AlertDialog.Builder adb = new AlertDialog.Builder(this);
-            adb.setMessage("New Batch has been recorded");
-            adb.setCancelable(true);
-            adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        if( isBatchNumValid && isExpDateValid && isQtyAdminValid && isQtyAvailVaild) {
+            AlertDialog.Builder ADBuilder1 = new AlertDialog.Builder(this);
+            ADBuilder1.setMessage("Batch has been recorded");
+            ADBuilder1.setCancelable(true);
+            ADBuilder1.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.cancel();
@@ -163,11 +158,13 @@ public class SelectedVid extends AppCompatActivity {
                     startActivity(intent);
                 }
             });
-            AlertDialog ad = adb.create();
-            ad.setTitle("Batch Added");
-            ad.show();
-        } else{
-            Snackbar.make(findViewById(R.id.constraint_layout_vaccination),"Field Empty!", BaseTransientBottomBar.LENGTH_INDEFINITE)
+            AlertDialog alert11 = ADBuilder1.create();
+            alert11.setTitle("Batch Record Successful");
+            alert11.show();
+
+        }
+        else{
+            Snackbar.make(findViewById(R.id.constraint_layout_vaccination),"Please fill in all the fields !", BaseTransientBottomBar.LENGTH_INDEFINITE)
                     .setAction("Close", new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -176,4 +173,3 @@ public class SelectedVid extends AppCompatActivity {
                     }).show();
         }
     }
-}
